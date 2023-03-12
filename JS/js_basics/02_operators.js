d20023130ffc396d138ac9012280037153d8ec44
Virendra patel <patelvirendra62@gmail.com>
2023-03-12 23:06:33 +0530

    added operators

---
+++ JS/js_basics/02_operators.js
@@ -0,0 +1,82 @@
+console.log("Operators.js");
+
+var name = "Virendra";
+
+var a = 90;
+var b = 91;
+
+var c = a + b;
+
+console.log(c);
+
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(2 ** 3);
+
+console.log(10 % 2);
+console.log(10 / 2);
+
+var v = 90;
+v++;
+v--;
+console.log(v);
+
+var c = 90;
+c = -c;
+console.log(c);
+c = +c;
+console.log(c);
+
+var number = 10;
+// number = number + 1;
+// number++;
+number += 2;
+
+console.log(number);
+
+console.clear();
+console.log("Conparision Op");
+
+// console.log(1 == 2);
+// console.log(1 == 1);
+// console.log(1 === 1);
+console.log(1 == "1"); //
+console.log(1 === "1"); // checks the Datatype
+
+console.log(1 < 9);
+console.log(1 > 9);
+
+console.log(1 >= 1);
+console.log(1 <= 2);
+
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+
+console.log(false || false);
+console.log(false || true);
+console.log(true || true);
+
+const marks = 45;
+
+console.log(marks > 30 && marks < 60);
+
+console.clear();
+
+console.log("Virendra" + "Patel");
+console.log("Virendra" - "Patel");
+console.log("Virendra" / "Patel");
+console.log("Virendra" * "Patel");
+
+var name = "A";
+
+name += "B";
+
+console.log(name);
+
+console.log(typeof 1);
+console.log(typeof "");
+console.log(typeof []);
+console.log(typeof 0);
+console.log(typeof NaN);
