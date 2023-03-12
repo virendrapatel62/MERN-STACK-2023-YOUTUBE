5fd4c4a0d965057362426d69ebf136082282b85d
Virendra patel <patelvirendra62@gmail.com>
2023-03-13 00:04:35 +0530

    added loops and if-else

---
+++ JS/js_basics/03_if_else.js
@@ -0,0 +1,13 @@
+var isLiked = true;
+
+if (isLiked) {
+  console.log("Show Liked Icon");
+}
+
+var number = 11;
+
+if (number % 2 == 0) {
+  console.log("This is Even Number");
+} else {
+  console.log("Number is Odd");
+}
---
+++ JS/js_basics/04_loops.js
@@ -0,0 +1,9 @@
+console.log("Loops");
+
+for (var i = 0; i < 5; i++) {
+  console.log("Hello World", i);
+}
+
+for (let i = 10; i < 20; i += 2) {
+  console.log(i);
+}
