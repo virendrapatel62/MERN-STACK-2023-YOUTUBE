f067eabbc1339f5d7f513743b823c088699282fd
Virendra patel <patelvirendra62@gmail.com>
2023-03-25 14:08:24 +0530

    var and scopes

---
+++ JS/js_basics/09_date.js
@@ -0,0 +1,21 @@
+var date = new Date();
+console.log(typeof date);
+
+console.log(date);
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+
+console.log(date.getTime());
+console.log(Date.now());
+
+console.log(53 * 365 * 24 * 60 * 60 * 1000);
+
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleTimeString());
---
+++ JS/js_basics/10_scope.js
@@ -0,0 +1,19 @@
+var username = "virendra";
+
+function sayHello() {
+  var username = "Harsh";
+  console.log(username);
+
+  console.log(myName);
+
+  for (var key in [1, 3]) {
+    var myName = "Virendra Kumar";
+    console.log(key, username);
+  }
+
+  console.log(key, myName);
+}
+
+sayHello();
+
+console.log(username);
